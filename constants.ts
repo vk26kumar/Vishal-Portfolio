0b808053e7adcc31c445f03c09541cbe87e4e223
Vishal Kumar <vkumar26062003@gmail.com>
2025-07-25 18:26:14 +0530

    solve

--- constants.ts
+++ constants.ts
@@ -19,7 +19,7 @@ export const PORTFOLIO_DATA: PortfolioData = {
     email: "vkumar26062003@gmail.com",
     linkedin: "https://linkedin.com/in/vishal-kumar2606",
     github: "https://github.com/vk26kumar",
-    resume: "https://drive.google.com/file/d/1I6ZVffGRXJ589ZjZ0joUi6DQsQT49R-7/view?usp=sharing",
+    resume: "https://drive.google.com/file/d/19RsZVbE4aZpjqrC_1C_gI9gMx7_FK5hg/view?usp=sharing",
   },
   academicDetails: [
     {
